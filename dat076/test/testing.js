625d96dda62bf927201b77e7466d15b5117af4a4
Tasdi <tasdikul@student.chalmers.se>
2017-03-12 17:47:35 +0100

    More testing
    Created functions to test 'get' of register and homepage

--- dat076/test/testing.js
+++ dat076/test/testing.js
@@ -3,9 +3,11 @@ var assert = require('assert');
 var expect = require('chai').expect;
 var User = require('../models/user');
 var Booking = require('../models/bookings');
-//Describe test
+var request = require('supertest');
+var app = require('../app');
+
 describe('test cases', function(){
-    //Create tests
+    //Create test for user
     it('should be invalid if attributes are empty', function(done){
         var user = new User();
         user.validate(function(err){
@@ -17,6 +19,7 @@ describe('test cases', function(){
         });
     });
 
+    //Create test for booking
     it('another test', function(done){
         var booking = new Booking();
         booking.validate(function(err){
@@ -26,6 +29,21 @@ describe('test cases', function(){
             expect(err.errors.patient).to.exist;
             done();
         });
-        
+    });
+});
+
+describe('Routing tests', function(){
+    //Testing 'get' of homepage
+    it('Testing get function of homepage', function(done){
+        request(app).get('/')
+        .expect(200)
+        .expect(/MedicalClinic/, done);
+    });
+
+    //Testing 'get' of registration page
+    it('Testing get function of registration', function(done) {
+        request(app).get('/register')
+        .expect(200)
+        .expect(/Register/, done);
     });
 });
